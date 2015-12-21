c3c490732e0eef897fdc10c7318008d4c2189088
Andrew Hunt <andrew@aghstrategies.com>
2015-12-21 15:56:03 -0500

    limit permissions for event edit forms

--- eventpermissions.php
+++ eventpermissions.php
@@ -3,6 +3,86 @@
 require_once 'eventpermissions.civix.php';
 
 /**
+ * Implements hook_civicrm_permission().
+ */
+function eventpermissions_civicrm_permission(&$permissions) {
+  $permissions += array(
+    'edit all events' => array(
+      ts('Edit all events', array('domain' => 'com.aghstrategies.eventpermissions')),
+      ts('Without this permission, users with Access CiviEvent may only edit events they created or where they are registered as Host.', array('domain' => 'com.aghstrategies.civimonitor')),
+    ),
+  );
+}
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * Note: the event "edit" ACL actually governs the ability to register for an
+ * event, so it isn't applicable here.  Instead, you can manually grant access
+ * to manage an event by making someone registered as "host".
+ */
+function eventpermissions_civicrm_preProcess($formName, &$form) {
+  switch ($formName) {
+    case 'CRM_Event_Form_ManageEvent_EventInfo':
+    case 'CRM_Event_Form_ManageEvent_Location':
+    case 'CRM_Event_Form_ManageEvent_Fee':
+    case 'CRM_Event_Form_ManageEvent_Registration':
+    case 'CRM_Friend_Form_Event':
+    case 'CRM_Event_Form_ManageEvent_ScheduleReminders':
+    case 'CRM_Event_Form_ManageEvent_Repeat':
+    case 'CRM_Event_Form_ManageEvent_Conference':
+      // Admins or users with "edit all events" can edit all events.
+      if (CRM_Core_Permission::check('edit all events') || CRM_Core_Permission::check('administer CiviCRM')) {
+        // return;
+      }
+      if ($form->_id) {
+        $contactId = CRM_Core_Session::singleton()->get('userID');
+
+        // Creators of events can edit their events.
+        try {
+          $result = civicrm_api3('Event', 'getcount', array(
+            'id' => $form->_id,
+            'created_id' => $contactId,
+          ));
+          if (!empty($result)) {
+            return;
+          }
+        }
+        catch (CiviCRM_API3_Exception $e) {
+          $error = $e->getMessage();
+          CRM_Core_Error::debug_log_message(ts('API Error finding event owner: %1', array(
+            'domain' => 'com.aghstrategies.eventpermissions',
+            1 => $error,
+          )));
+        }
+
+        // Hosts of events can edit their events.
+        try {
+          // TODO: fix role_id depending upon site-specific naming.
+          $result = civicrm_api3('Participant', 'getcount', array(
+            'contact_id' => $contactId,
+            'event_id' => $form->_id,
+            'role_id' => "Host",
+          ));
+          if (!empty($result)) {
+            return;
+          }
+        }
+        catch (CiviCRM_API3_Exception $e) {
+          $error = $e->getMessage();
+          CRM_Core_Error::debug_log_message(ts('API Error finding event owner: %1', array(
+            'domain' => 'com.aghstrategies.eventpermissions',
+            1 => $error,
+          )));
+        }
+        // Deny access.
+        CRM_Core_Error::statusBounce(ts('You do not have access to edit this event (ID: %1).', array(1 => $form->_id, 'domain' => 'com.aghstrategies.eventpermissions')));
+      }
+      break;
+  }
+}
+
+/**
  * Implements hook_civicrm_config().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
