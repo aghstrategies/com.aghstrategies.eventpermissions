1ed3cd8a0b11ab0edf37920a9e697e2a9ef012a8
Andrew Hunt <andrew@aghstrategies.com>
2015-12-22 12:32:34 -0500

    cleanup of widget

--- CRM/Eventpermissions/Page/MyEvents.php
+++ CRM/Eventpermissions/Page/MyEvents.php
@@ -4,24 +4,18 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Eventpermissions_Page_MyEvents extends CRM_Core_Page {
   public function run() {
-    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-    CRM_Utils_System::setTitle(ts('My Events', array('domain' => 'com.aghstrategies.eventpermissions')));
-
+    // Get link options for managing events.
     $enableCart = CRM_Core_BAO_Setting::getItem(CRM_Core_BAO_Setting::EVENT_PREFERENCES_NAME,
       'enable_cart'
     );
-
-    // Get link options for managing events.
     $tabs = CRM_Event_Page_ManageEvent::tabs($enableCart);
 
     foreach ($tabs as $tab => $tabInfo) {
       $tabs[$tab]['name'] = $tabInfo['title'];
-      $tabs[$tab]['qs'] = 'reset=1&id=%%id%%';
+      $tabs[$tab]['qs'] = ($tab == 'reminder') ? 'reset=1&action=browse&id=%%id%%' : 'reset=1&action=update&id=%%id%%';
     }
 
-
-    // Example: Assign a variable for use in a template
-    $utils = new CRM_Eventpermissions_Utils;
+    $utils = new CRM_Eventpermissions_Utils();
     $events = array();
     foreach ($utils->myUpcomingEvents() as $id => $title) {
       $events[] = array(
